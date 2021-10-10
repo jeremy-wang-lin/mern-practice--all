8cf2f03330107532e2bcf6a93a0be382eb0761bc
jeremy-wang-lin <51841335+jeremy-wang-lin@users.noreply.github.com>
2021-10-10 15:00:26 +0800

    Finished in 57. Managing State in the Input Component
    Use the useReducer hook to manage complex state (or interconnected state). In this example, the states are inputed value and validation result.

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -4,9 +4,17 @@ import Input from "../../shared/components/FormElements/Input";
 import "./NewPlace.css";
 
 const NewPlace = () => {
-  return <form className="place-form">
-    <Input element="input" type="text" label="Title" />
-  </form>;
-}
+  return (
+    <form className="place-form">
+      <Input
+        element="input"
+        type="text"
+        label="Title"
+        validators={[]}
+        errorText="Please enter a valid title."
+      />
+    </form>
+  );
+};
 
 export default NewPlace;
