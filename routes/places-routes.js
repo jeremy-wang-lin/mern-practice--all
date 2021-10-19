839084d27ce20e3246df6a5e9e17aece0ca29c1c
jeremy-wang-lin <51841335+jeremy-wang-lin@users.noreply.github.com>
2021-10-19 23:18:20 +0800

    Finished in 89. Adding Place-Specific Routes

--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,10 +1,30 @@
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get('/', (req, res, next) => {
-  console.log('GET Request in Places');
-  res.json({message: 'It works!'});
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Taipei 101",
+    description: "One of the most famous landmarks in Taiwan",
+    location: {
+      lat: 25.0338089,
+      lng: 121.5623674,
+    },
+    address: "110台北市信義區信義路五段7號",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid; // { pid: p1 }
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+
+  console.log("GET Request in Places");
+
+  res.json({place}); // => { place } => { place: place }
 });
 
 module.exports = router;
