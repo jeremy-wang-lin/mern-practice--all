32c690fcb92f2dcec0879a49f04b5e97f4a84540
jeremy-wang-lin <51841335+jeremy-wang-lin@users.noreply.github.com>
2021-10-23 02:41:02 +0800

    Finished in 101. Validating Patch Requests & User Routes

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 
 const usersControllers = require("../controllers/users-controllers");
 
@@ -6,7 +7,15 @@ const router = express.Router();
 
 router.get("/", usersControllers.getAllUsers);
 
-router.post("/signup", usersControllers.signUp);
+router.post(
+  "/signup",
+  [
+    check("name").isLength({ min: 4 }),
+    check("email").normalizeEmail().isEmail(),
+    check("password").isLength({ min: 8 }),
+  ],
+  usersControllers.signUp
+);
 
 router.post("/login", usersControllers.login);
 
