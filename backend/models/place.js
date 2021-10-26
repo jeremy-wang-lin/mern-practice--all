919dcb0e75a51feb8644250e319f721425e28497
jeremy-wang-lin <51841335+jeremy-wang-lin@users.noreply.github.com>
2021-10-26 15:47:44 +0800

    Finished in 124. Creating the Place Schema & Model

---
+++ backend/models/place.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const placeSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  image: { type: String, required: true },
+  address: { type: String, required: true },
+  location: {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+  },
+  creator: { type: String, required: true },
+});
+
+module.exports = mongoose.model("Place", placeSchema);
+// mongoose.model("Place", placeSchema) will return a construction function.
+// 第一個參數 "Place" (慣例是首字大寫，單數形式), 將會轉成 "places" 這個 collection 名稱 (首字換小寫，字尾加複數s)
